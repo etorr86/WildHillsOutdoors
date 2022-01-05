cbd9bb5d38546b79a7193101ea98d5ba61571f7e
etorr86 <66689507+etorr86@users.noreply.github.com>
2022-01-05 11:15:51 -0500

    used context to update basket and cart count

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,7 +1,7 @@
 import { ThemeProvider } from "@emotion/react";
 import { Container, CssBaseline } from "@mui/material";
 import { createTheme } from '@mui/material/styles';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import AboutPage from "../../features/about/AboutPage";
@@ -11,15 +11,34 @@ import ProductDetailsPage from "../../features/catalog/ProductDetails";
 import ContactPage from "../../features/contact/ContactPage";
 import HomePage from "../../features/home/HomePage";
 import Header from "./Header";
+import { getCookie } from "../util/util";
 import { injectStyle } from "react-toastify/dist/inject-style";
 import ServerError from "../errors/ServerError";
 import NotFound from "../errors/NotFound";
+import { useStoreContext } from "../context/StoreContext";
+import agent from "../api/agent";
+import LoadingComponent from "./LoadingComponent";
 
 
 function App() {
   // "TOAST" CALL IT ONCE IN YOUR APP
   injectStyle();
-  
+
+  const { setBasket } = useStoreContext();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const buyerId = getCookie('buyerId');
+    if (buyerId) {
+      agent.Basket.get()
+        .then(basket => setBasket(basket))
+        .catch(error => console.log(error))
+        .finally(() => setLoading(false))
+    } else {
+      setLoading(false);
+    }
+  }, [setBasket])
+
   const [darkMode, setDarkMode] = useState(false);
   const paletteType = darkMode ? 'dark' : 'light';
   const theme = createTheme({
@@ -35,9 +54,11 @@ function App() {
     setDarkMode(!darkMode);
   }
 
+  if (loading) return <LoadingComponent message="Initializing app..." />
+
   return (
     <ThemeProvider theme={theme}>
-      <ToastContainer position="bottom-right" hideProgressBar theme='colored'/>
+      <ToastContainer position="bottom-right" hideProgressBar theme='colored' />
       <CssBaseline></CssBaseline>
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
@@ -47,9 +68,9 @@ function App() {
           <Route exact path='/catalog' component={Catalog} />
           <Route path='/catalog/:id' component={ProductDetailsPage} />
           <Route path='/contact' component={ContactPage} />
-          <Route path='/server-error' component={ServerError}/>
-          <Route path='/basket' component={BasketPage}/>
-          <Route component={NotFound}/>
+          <Route path='/server-error' component={ServerError} />
+          <Route path='/basket' component={BasketPage} />
+          <Route component={NotFound} />
         </Switch>
       </Container>
     </ThemeProvider>
